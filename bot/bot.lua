4a623782ae886e1252ed2aa72b581d8054e79c08
Nakhlok1 <darkhal93@gmail.com>
2016-11-17 14:45:09 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -230,7 +230,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {257000294},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
